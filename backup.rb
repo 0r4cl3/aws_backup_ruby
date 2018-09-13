3aea5fa672e892e2f7b15943722f69f05b952849
0r4cl3 <lucalicheri@gmail.com>
2018-09-13 21:37:30 +0200

    correct typos

--- backup.rb
+++ backup.rb
@@ -5,17 +5,17 @@ ec2 = Aws::EC2::Resource.new(region: 'eu-central-1')
 #Add the number of days you want to keep the backup
 retention_days = 2
 
-delete_on = Time.now.utc + (60 * 60 * 24 * retetion_days)
+delete_on = Time.now.utc + (60 * 60 * 24 * retention_days)
 
 ec2.volumes.each do |volume|
   result = volume.tags.find do |tag|
-    tag == "Name"
+    tag.key == "Name"
   end
   tag_name = result.value
 
   snapshot = ec2.create_snapshot ({
     description: "Snapshot created automatically using the aws_backup script https://github.com/0r4cl3/aws_backup_ruby",
     volume_id: volume.id,
-    tag_specifications: [{resource_type: 'snapshot', tags: [{key: "Name", value: tag_name}, {key: "delete_backup_on", value: delete_on}]}]
+    tag_specifications: [{resource_type: 'snapshot', tags: [{key: "Name", value: tag_name}, {key: "delete_backup_on", value: delete_on.to_s}]}]
   })
 end
