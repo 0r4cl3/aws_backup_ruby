07a53068355258f3fdcebf1af9890031ea2a5c38
0r4cl3 <lucalicheri@gmail.com>
2018-09-15 23:01:35 +0200

    fixing code

--- prune.rb
+++ prune.rb
@@ -2,10 +2,14 @@ require 'aws-sdk-ec2'
 
 ec2 = Aws::EC2::Resource.new(region: 'eu-central-1')
 
-ec2.snapshots.each do |snapshot|
+result = ec2.snapshots.select do |snapshot|
   result = snapshot.tags.find do |tag|
     tag.key == "delete_backup_on"
   end
-  puts result
+  !!result
+end
+
+result.each do |i|
+  puts i.inspect
 end
 
